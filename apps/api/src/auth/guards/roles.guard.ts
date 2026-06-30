7a3d1101070c8db9e78aaf1f1cc7e6944fa57e3c
mazharul90007 <mazharul90007@gmail.com>
2026-06-30 13:13:07 +0600

    Role guard has been created

---
+++ apps/api/src/auth/guards/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtPayload, UserRole } from '@vojon/types';
+import { Request } from 'express';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context
+      .switchToHttp()
+      .getRequest<Request & { user: JwtPayload }>();
+
+    if (!user) {
+      throw new ForbiddenException('Forbidden');
+    }
+
+    return requiredRoles.includes(user.role);
+  }
+}
